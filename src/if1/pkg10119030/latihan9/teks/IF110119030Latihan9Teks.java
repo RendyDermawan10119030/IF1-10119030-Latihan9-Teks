491b4ed74e851383d5bbfdbf874a27c4799c0f0f
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:48:54 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan9/teks/IF110119030Latihan9Teks.java
@@ -0,0 +1,18 @@
+package if1.pkg10119030.latihan9.teks;
+/**
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Teks
+ */
+public class IF110119030Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c );
+    }
+    
+}
